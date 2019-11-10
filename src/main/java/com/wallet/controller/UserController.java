50f2f840f056b244a591ac38deb21ce2eaae6f4f
amycardoso <alice.cardoso10@gmail.com>
2019-11-10 14:06:26 -0500

    Added Bcrypt and removed password attribute from response payload

--- src/main/java/com/wallet/controller/UserController.java
+++ src/main/java/com/wallet/controller/UserController.java
@@ -15,6 +15,7 @@ import com.wallet.dto.UserDTO;
 import com.wallet.model.User;
 import com.wallet.response.Response;
 import com.wallet.service.UserService;
+import com.wallet.util.Bcrypt;
 
 @RestController
 @RequestMapping("user")
@@ -43,8 +44,7 @@ public class UserController {
 		u.setId(dto.getId());
 		u.setEmail(dto.getEmail());
 		u.setName(dto.getName());
-		u.setPassword(dto.getPassword());
-
+		u.setPassword(Bcrypt.getHash(dto.getPassword()));
 		return u;
 	}
 
@@ -53,8 +53,7 @@ public class UserController {
 		dto.setId(u.getId());
 		dto.setEmail(u.getEmail());
 		dto.setName(u.getName());
-		dto.setPassword(u.getPassword());
-
+		
 		return dto;
 	}
 }
