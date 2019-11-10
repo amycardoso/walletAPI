4bf596624173a09ba34e9a70ec19cd4ab9df1ab4
amycardoso <alice.cardoso10@gmail.com>
2019-11-10 11:18:28 -0500

    UserRepositoryTest, UserRepository and User

---
+++ src/main/java/com/wallet/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wallet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wallet.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
